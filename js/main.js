4d034ec374e6ff8990c4e3fba33baed7861d8a84
Dim <dcaramello@gmail.com>
2020-09-03 09:41:47 +0200

    add button start for starting game

--- js/main.js
+++ js/main.js
@@ -3,33 +3,35 @@ let card = document.getElementsByClassName("card");
 
 // on recupere les elements blocCards de html et on les stock dans un array
 let arrayCards = [];
-let blocCards = document.getElementById("blocCards");
 
-for (let i = 0 ; i < 12; i ++) {
+for (let i = 0 ; i < card.length; i ++) {
     arrayCards.push(card[i]);
 }
 
 // un array d'images
 let arrayPictures = [
-    "img/boohan.webp",
-    "img/broly.webp",
-    "img/freezer.webp",
-    "img/migate.webp",
-    "img/piccolo.webp",
-    "img/rosé.webp",
-    "img/boohan.webp",
-    "img/broly.webp",
-    "img/freezer.webp",
-    "img/migate.webp",
-    "img/piccolo.webp",
-    "img/rosé.webp"
+    "img/boohan.jpg",
+    "img/broly.jpg",
+    "img/freezer.jpg",
+    "img/migate.jpg",
+    "img/piccolo.jpg",
+    "img/rosé.jpg",
+    "img/boohan.jpg",
+    "img/broly.jpg",
+    "img/freezer.jpg",
+    "img/migate.jpg",
+    "img/piccolo.jpg",
+    "img/rosé.jpg"
 ];
 
 let result = [];
 let clic = 0;
+let score = 0;
 
-function getImages() {
-// boucle for qui parcourt tous les elements et au clic remplace l'image
+
+function game() {
+    
+    // boucle for qui parcourt tous les elements et au clic remplace l'image
     for(let i = 0; i < arrayCards.length; i ++) {   
         arrayCards[i].addEventListener('click', function() {
             if (arrayCards[i]) {      
@@ -42,28 +44,61 @@ function getImages() {
 
             // au deuxieme clic
             if (clic === 2) { 
-                    if (result[0].src === result[1].src) {
-                        alert("Nickel ça !");
-                        result=[];
-                        console.log(result);
-                    }
-                    else {
-                        setTimeout(noPair, 1000);
-                    }
-                clic = 0;
+                if (result[0].src === result[1].src) {
+                    score ++;
+                    console.log("score = " + score);
+                    alert("Nickel ça !");
+                    result=[];
+                    
+                    if (score === 2) {
+                        alert("win");
+                        window.location.reload();
+                        play();
+                    }  
+                }
+                else {
+                    setTimeout(noPair, 1000);
+                }
+                clic = 0;     
             }
         }); 
-    }       
+    }    
 }
-getImages();
-
 
 // fonction qui remet les images a satan.webp si non paire
 function noPair () {
-    result[0].src = "img/satan.webp";
-    result[1].src = "img/satan.webp";
-result=[];
-console.log(result);
+    result[0].src = "img/satan.jpg";
+    result[1].src = "img/satan.jpg";
+    result=[];
 }
 
+
+function play() {
+    let start = document.getElementById("start");
+    start.addEventListener('click', function (){
+        game();
+    });
+}
+
+
+
+
+
+play();
+
+
+// Fonction qui lance le jeu
+// function play() {
+//     score = 0;
+//     result = [];
+//     clic = 0;
+//     let play = prompt("taper play pour jouer");
+//     if (play === "play") {
+//         for (let i = 0; i < arrayCards.length; i++) {
+//             arrayCards[i].src = "img/satan.webp";
+//         }
+        
+//         game();
+//     }
+// }
 
